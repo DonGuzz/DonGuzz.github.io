d64b5de3151b8f551a4a58275318366f6e9c4153
DonGuzz <73078364+DonGuzz@users.noreply.github.com>
2021-07-19 23:48:16 -0300

    Create contato.js
    Criação do js que processa o forms da pagina contato

---
+++ js/contato.js
@@ -0,0 +1,29 @@
+function ContatoC() {
+    var cliente = {
+        nome: "",
+        email: "",
+        telefone: "",
+        servico: "",
+        descricao: ""
+    }
+
+    cliente.nome = document.getElementById("nome").value;
+    cliente.email = document.getElementById("email").value;
+    cliente.telefone = document.getElementById("telef").value;
+    cliente.servico = document.getElementById("serv").value;
+    cliente.descricao = document.getElementById("ped").value;
+
+    var confirm = prompt("Confirme o dados abaixo por favor.\nNome: " + cliente.nome + "\nEmail: " + cliente.email + "\nTelefone: " + cliente.telefone + "\nCaso esteja tudo OK, digite Sim, caso contrario, Não.");
+
+    if (confirm.toUpperCase() == "SIM") {
+        alert("Obrigado pela mensagem " + cliente.nome + "!\nLogo entro em contato contigo!");
+    }
+    else if (confirm.toUpperCase() == "NÃO") {
+        alert("Preencha de novo por favor.");
+    }
+    else {
+        alert("Resposta invalida!");
+    }
+
+
+}
