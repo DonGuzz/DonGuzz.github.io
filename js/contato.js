ab27b139b64ac9151855e8384c78f7213bc79407
DonGuzz <73078364+DonGuzz@users.noreply.github.com>
2021-07-20 22:00:48 -0300

    Update contato.html e .js
    Corrigindo erros internos da validação via js e corrigindo mal funcionamento das opções em ratio

--- js/contato.js
+++ js/contato.js
@@ -1,4 +1,5 @@
 function ContatoC() {
+
     var cliente = {
         nome: "",
         email: "",
@@ -10,21 +11,43 @@ function ContatoC() {
     cliente.nome = document.getElementById("nome").value;
     cliente.email = document.getElementById("email").value;
     cliente.telefone = document.getElementById("telef").value;
-    cliente.servico = document.getElementById("serv").value;
+    cliente.servico = document.getElementsByName("serv").value;
     cliente.descricao = document.getElementById("ped").value;
 
-    var confirm = prompt("Confirme o dados abaixo por favor.\nNome: " + cliente.nome + "\nEmail: " + cliente.email + "\nTelefone: " + cliente.telefone + "\nCaso esteja tudo OK, digite Sim, caso contrario, Não.");
+    if (cliente.nome == "") {
+        window.alert("Preencha o campo nome!");
+        cliente.nome.focus();
+        return false;
+    } else if (cliente.email == "") {
+        window.alert("Preencha o campo email!");
+        cliente.email.focus();
+        return false;
+    } else if (cliente.telefone == "") {
+        window.alert("Preencha o campo telefone!");
+        cliente.telefone.focus();
+        return false;
+    } else if (cliente.servico == "") {
+        window.alert("Preencha o campo serviço!");
+        cliente.servico.focus();
+        return false;
+    } else if (cliente.descricao == "") {
+        window.alert("Preencha o campo descrição!");
+        cliente.descricao.focus();
+        return false;
+    } else {
 
-    if (confirm.toUpperCase() == "SIM") {
-        alert("Obrigado pela mensagem " + cliente.nome + "!\nLogo entro em contato contigo!");
-    }
-    else if (confirm.toUpperCase() == "NÃO") {
-        alert("Preencha de novo por favor.");
-    }
-    else {
-        alert("Resposta invalida!");
-    }
+        var confirm = prompt("Confirme o dados abaixo por favor.\nNome: " + cliente.nome + "\nEmail: " + cliente.email + "\nTelefone: " + cliente.telefone + "\nCaso esteja tudo OK, digite Sim, caso contrario, Não.");
 
-    location.reload()
+        if (confirm.toUpperCase() == "SIM") {
+            alert("Obrigado pela mensagem " + cliente.nome + "!\nLogo entro em contato contigo!");
+        }
+        else if (confirm.toUpperCase() == "NÃO") {
+            alert("Preencha de novo por favor.");
+        }
+        else {
+            alert("Resposta invalida!");
+        }
 
+        location.reload()
+    }
 }
